76e35ffd0d669cde046f64f9b707a3620faf8b0d
LadyKillerr <dungvuduc0509@gmail.com>
2023-06-08 13:57:22 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+
+var headingElement = document.querySelector('.heading');
+
+    console.log(headingElement.innerText);
+    console.log(headingElement.textContent);
+
+headingElement.textContent = `
+
+Dũng đẹp trai
+
+`;
+
+console.log(headingElement)
+ */
+
+// console.log(headingNodes)
